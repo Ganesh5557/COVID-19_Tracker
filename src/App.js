e7f1409eb091fd6da4d1c27870a0eaabba1a88e6
Ganesh5557 <ganesh.pandey5557@gmail.com>
2020-05-05 21:07:09 -0400

    updated the title

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends React.Component {
         return (
             <div className={styles.container}>
                 {/* <img className={styles.image} src={image} alt="COVID-19" /> */}
+                <h1> COVID-19</h1>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
                 <Chart data={data} country={country} />
